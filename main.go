bb78593ba5d2169f4c2e8b318defc28cf9111c39
fengshaomin <fengshaomin@bjsasc.com>
2018-03-08 18:29:43 +0800

    用来用去还是goland好用

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"os/exec"
 
+	"io/ioutil"
 )
 
 func main() {
 
-
-
-}
+	var cmd= exec.Command("dir")
+		var a,_=cmd.StdoutPipe()
+		defer a.Close()
+		var v,_ = ioutil.ReadAll(a)
+		println(string(v))
+	}

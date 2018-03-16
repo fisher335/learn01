f6fe2022e1e8ae1313f05068c9d18704439215a1
fengshaomin <shaomina1984>
2018-03-17 01:26:39 +0800

    goquery没有bs4还用

--- main.go
+++ main.go
@@ -1,31 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"strconv"
+	"fmt"
 	"strings"
 )
 
-type Person struct {
-	Name string
-	Age  int
-	Part string
+type Herf struct {
+	Type    string
+	Content string
+	Img     string
 }
 
 func main() {
 
-	//var c = grequests.RequestOptions{Headers: map[string]string{"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}}
+	re := GetContent()
+	println("--------------------------------------------------------------")
+	print(re[1].Content)
+	for a, value := range re {
+		print(strconv.Itoa(a) + "---" + value.Content)
+	}
 
+}
+
+func GetContent() []Herf {
+	c := make([]Herf, 0)
 	var url = "https://www.qiushibaike.com/"
 	doc, _ := goquery.NewDocument(url)
 	doc.Find("div[id^='qiushi']").Each(func(i int, selection *goquery.Selection) {
+		println(i)
+
 		s := selection.Find("div.content").Find("span").Text()
+		//fmt.Printf(s)
+		if s != "" {
+			var d = Herf{}
+			d.Content = s
 
-		fmt.Printf(s)
+			src, _ := selection.Find("img.illustration").Attr("src")
+			d.Img = "http:" + src
+			fmt.Println(strings.Replace(src, "//", "http://", -1))
 
-		src, _ := selection.Find("img.illustration").Attr("src")
-		fmt.Println(strings.Replace(src,"//","http://",-1))
 
+			c = append(c, d)
+		}
 	})
-
+	return c
 }

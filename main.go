091a4c15ed55e3f24aedc335e3121fda6d1b8705
fengshaomin <fengshaomin@bjsasc.com>
2018-03-19 11:05:47 +0800

    用来用去还是goland好用

--- main.go
+++ main.go
@@ -1,49 +1,15 @@
 package main
 
 import (
-	"github.com/PuerkitoBio/goquery"
-	"strconv"
-	"fmt"
-	"strings"
+
+
 )
 
-type Herf struct {
-	Type    string
-	Content string
-	Img     string
-}
+
 
 func main() {
 
-	re := GetContent()
-	println("--------------------------------------------------------------")
-	print(re[1].Content)
-	for a, value := range re {
-		print(strconv.Itoa(a) + "---" + value.Content)
-	}
+
 
 }
 
-func GetContent() []Herf {
-	c := make([]Herf, 0)
-	var url = "https://www.qiushibaike.com/"
-	doc, _ := goquery.NewDocument(url)
-	doc.Find("div[id^='qiushi']").Each(func(i int, selection *goquery.Selection) {
-		println(i)
-
-		s := selection.Find("div.content").Find("span").Text()
-		//fmt.Printf(s)
-		if s != "" {
-			var d = Herf{}
-			d.Content = s
-
-			src, _ := selection.Find("img.illustration").Attr("src")
-			d.Img = "http:" + src
-			fmt.Println(strings.Replace(src, "//", "http://", -1))
-
-
-			c = append(c, d)
-		}
-	})
-	return c
-}
---
+++ qiushibaike.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"fmt"
+	"strings"
+)
+
+type Herf struct {
+	Type    string
+	Content string
+	Img     string
+}
+
+
+func GetContent() []Herf {
+	c := make([]Herf, 0)
+	var url = "https://www.qiushibaike.com/"
+	doc, _ := goquery.NewDocument(url)
+	doc.Find("div[id^='qiushi']").Each(func(i int, selection *goquery.Selection) {
+		println(i)
+
+		s := selection.Find("div.content").Find("span").Text()
+		//fmt.Printf(s)
+		if s != "" {
+			var d = Herf{}
+			d.Content = s
+
+			src, _ := selection.Find("img.illustration").Attr("src")
+			d.Img = "http:" + src
+			fmt.Println(strings.Replace(src, "//", "http://", -1))
+
+
+			c = append(c, d)
+		}
+	})
+	return c
+}
